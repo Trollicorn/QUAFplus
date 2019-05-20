fc5487ee45690bc4c5bd64a0b3586f6e65c011f2
Theodore Peters <tpeters@stuy.edu>
2019-05-20 12:34:39 -0400

    python code execution now working for multiple code snippets in the same webpage

--- QUAFplus/static/code_snippets.js
+++ QUAFplus/static/code_snippets.js
@@ -1,16 +1,28 @@
 document.addEventListener("DOMContentLoaded",()=>{
     var out=(e)=>{
-	return(t)=>{
-	e.getElementsByClassName("code_out")[0].innerHTML=t;
-	};
+		return(t)=>{
+			e.getElementsByClassName("code_out").item(0).innerHTML+=t;
+		};
     };
-
+	py_snips=document.getElementsByClassName("py_snip");
+	for(var  i=0;i<py_snips.length;i++){
+		let q=py_snips.item(i);
+		console.log(q);
+		q.getElementsByClassName("code_run").item(0).addEventListener("click",()=>{
+			q.getElementsByClassName("code_out").item(0).innerHTML='';
+			Sk.configure({output:out(q),read:builtinRead});
+			try{
+				Sk.importMainWithBody("<stdin>",false,q.getElementsByClassName("code_in").item(0).value);
+			}catch(aaa){
+				q.getElementsByClassName("code_out").item(0).innerHTML=aaa;
+			}
+		});
+	}
     var builtinRead=(x)=>{
-	if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
-	    throw "File not found: '" + x + "'";
-	return Sk.builtinFiles["files"][x];
+		if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
+			throw "File not found: '" + x + "'";
+		return Sk.builtinFiles["files"][x];
     }
-
     /*
     var runit=()=>{
 	var prog = document.getElementById("yourcode").value;
