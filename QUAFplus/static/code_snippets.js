d0f81def3edf48a152e068446b9d3b57c6af975d
anotherLostKitten <tpeters@stuy.edu>
2019-05-28 13:24:01 -0400

    devlog later

--- QUAFplus/static/code_snippets.js
+++ QUAFplus/static/code_snippets.js
@@ -4,11 +4,10 @@ document.addEventListener("DOMContentLoaded",()=>{
 		    e.getElementsByClassName("code_out").item(0).innerHTML+=t.replace("\n","<br>");
 		};
     };
-	py_snips=document.getElementsByClassName("py_snip");
-	for(var  i=0;i<py_snips.length;i++){
-		let q=py_snips.item(i);
-		console.log(q);
-		q.getElementsByClassName("code_run").item(0).addEventListener("click",()=>{
+    var py_eventify=(e)=>{
+	let q=e;
+	console.log(q)
+	q.getElementsByClassName("code_run").item(0).addEventListener("click",()=>{
 			let cdo=q.getElementsByClassName("code_out").item(0);
 			cdo.innerHTML='';
 			Sk.configure({output:out(q),read:builtinRead});
@@ -20,8 +19,29 @@ document.addEventListener("DOMContentLoaded",()=>{
 				q.getElementsByClassName("code_out").item(0).innerHTML=aaa;
 			}
 			if(cdo.innerHTML==='')cdo.innerHTML='<br>';
-		});
-	}
+	});
+	let b=q.getElementsByClassName("code_delete")
+	if(b.length>0)
+	    b.item(0).addEventListener("click",()=>{
+		q.parentNode.removeChild(q);
+	    });
+    };
+    var mk_py_eventify=(e,daddy=document.getElementById("container"))=>{
+	let ok=document.createElement('div');
+	ok.setAttribute("class",'py_snip');
+	ok.innerHTML=document.getElementById("py_snip_edit_source").innerHTML;
+	daddy.appendChild(ok);
+    }
+    var py_snips=document.getElementsByClassName("py_snip");
+    for(var i=0;i<py_snips.length;i++){
+	py_eventify(py_snips.item(i));	
+    }
+
+    var mk_py_snips=document.getElementsByClassName("new_py_snip");
+    for(var i=0;i<mk_py_snips.length;i++){
+	mk_py_eventify(mk_py_snips.item(i));	
+    }
+    
     var builtinRead=(x)=>{
 		if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
 			throw "File not found: '" + x + "'";
