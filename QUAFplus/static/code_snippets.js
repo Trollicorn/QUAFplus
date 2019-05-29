dc6d394ba56b494e82c75d1e718f384e08283ddc
Theodore Peters <tpeters@stuy.edu>
2019-05-28 23:57:59 -0400

    old man says you are what you eat, eat well

--- QUAFplus/static/code_snippets.js
+++ QUAFplus/static/code_snippets.js
@@ -5,9 +5,9 @@ document.addEventListener("DOMContentLoaded",()=>{
 		};
     };
     var py_eventify=(e)=>{
-	let q=e;
-	console.log(q)
-	q.getElementsByClassName("code_run").item(0).addEventListener("click",()=>{
+		let q=e;
+		console.log(q)
+		q.getElementsByClassName("code_run").item(0).addEventListener("click",()=>{
 			let cdo=q.getElementsByClassName("code_out").item(0);
 			cdo.innerHTML='';
 			Sk.configure({output:out(q),read:builtinRead});
@@ -19,45 +19,60 @@ document.addEventListener("DOMContentLoaded",()=>{
 				q.getElementsByClassName("code_out").item(0).innerHTML=aaa;
 			}
 			if(cdo.innerHTML==='')cdo.innerHTML='<br>';
-	});
-	let b=q.getElementsByClassName("code_delete")
-	if(b.length>0)
-	    b.item(0).addEventListener("click",()=>{
-		q.parentNode.removeChild(q);
-	    });
+		});
+		let b=q.getElementsByClassName("code_delete")
+		if(b.length>0)
+			b.item(0).addEventListener("click",()=>{
+				q.parentNode.removeChild(q);
+			});
     };
-    var mk_py_eventify=(e,daddy=document.getElementById("container"))=>{
-	let ok=document.createElement('div');
-	ok.setAttribute("class",'py_snip');
-	ok.innerHTML=document.getElementById("py_snip_edit_source").innerHTML;
-	daddy.appendChild(ok);
-    }
+    var mk_py_eventify=(e,daddy=document.getElementById("code_edits"))=>{
+		e.addEventListener("click",()=>{
+			let ok=document.createElement('div');
+			ok.setAttribute("class",'py_snip');
+			ok.innerHTML=document.getElementById("py_snip_edit_source").innerHTML;
+			daddy.appendChild(ok);
+			py_eventify(ok);
+		});
+    };
     var py_snips=document.getElementsByClassName("py_snip");
     for(var i=0;i<py_snips.length;i++){
-	py_eventify(py_snips.item(i));	
+		py_eventify(py_snips.item(i));	
     }
 
     var mk_py_snips=document.getElementsByClassName("new_py_snip");
     for(var i=0;i<mk_py_snips.length;i++){
-	mk_py_eventify(mk_py_snips.item(i));	
+		mk_py_eventify(mk_py_snips.item(i));
     }
-    
     var builtinRead=(x)=>{
 		if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
 			throw "File not found: '" + x + "'";
 		return Sk.builtinFiles["files"][x];
-    }
-    /*
-    var runit=()=>{
-	var prog = document.getElementById("yourcode").value;
-	var mypre = document.getElementById("output");
-	mypre.innerHTML = '';
-	Sk.configure({output:outf,read:builtinRead});
-	try {
-	    Sk.importMainWithBody("<stdin>",false,prog);
-	} catch(e) {
-	    alert(e);
+    };
+	var stringify_snip=(e)=>{
+		let title=e.getElementsByClassName("code_title_inp").item(0);
+		let cde=e.getElementsByClassName("code_in").item(0);
+		let lang=e.getElementsByClassName("code_type").item(0);
+		return "{%{{{{"+lang.innerHTML.toLowerCase()+"\n"+title.value+"\n"+cde.value+"}}}}%}"
+	};
+	var snippify_string=(s,daddy)=>{
+		var r=s.indexOf("{%{{{{")
+		var r1=s.indexOf("\n",r);
+		var r2=s.indexOf("\n",r1+1);
+		var nd=s.indexOf("}}}}%}",r2);
+		if(r==-1||r1==-1||r2==-1||nd==-1)return;
+		var lang=s.substring(r+6,r1);
+		if(lang=="py"){
+			let ok=document.createElement('div');
+			ok.setAttribute("class","py_snip");
+			ok.innerHTML=document.getElementById("py_snip_source").innerHTML;
+			ok.getElementsByClassName("code_title_inp").item(0).innerHTML=s.substring(r1+1,r2);
+			ok.getElementsByClassName("code_in").item(0).innerHTML=s.substring(r2+1,nd);
+			py_eventify(ok);
+			daddy.appendChild(ok);
+		}
 	}
-    }
-    */
+	document.getElementById("stringify_test").addEventListener("click",()=>{
+		snippify_string(stringify_snip(document.getElementsByClassName("py_snip").item(0)),document.getElementById("code_edits"));
+	});
 });
