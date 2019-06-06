ece782706ff4bc4c9d6df97332094a991e189b02
Theodore Peters <tpeters@stuy.edu>
2019-06-06 15:39:39 -0400

    ;

--- QUAFplus/__init__.py
+++ QUAFplus/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, url_for, redirect, send_from_directory, request
 from flask_mail import Mail, Message
 from passlib.hash import sha256_crypt
-from QUAFplus import pp as pp
+try:
+    from QUAFplus import pp as pp
+except ModuleNotFoundError:
+    import pp
 import datetime
 #from util import database
 
@@ -40,9 +43,20 @@ def authenticate():
     """
     return render_template("signup.html")
 
-@app.route('/create')
+@app.route('/create',methods=["POST","GET"])
 def ok():
-    return render_template("create.html")
+    if request.method=="POST":
+        parent=request.form["parent"]if"parent"in request.form else 0
+        server=request.form["server"]if"server"in request.form else 0
+    else:
+        parent=request.args["parent"]if"parent"in request.args else 0
+        server=request.args["server"]if"server"in request.args else 0
+    return render_template("create.html",parent=parent,server=server)
+@app.route('/make_post',methods=["POST"])
+def okok():
+    print(request.form)
+    return redirect('/')
+    
 
 if __name__ == "__main__":
     app.debug = True
--- QUAFplus/static/code_snippets.js
+++ QUAFplus/static/code_snippets.js
@@ -1,79 +1,107 @@
 document.addEventListener("DOMContentLoaded",()=>{
     var out=(e)=>{
-	return(t)=>{
-	    e.getElementsByClassName("code_out").item(0).innerHTML+=t.replace("\n","<br>");
-	};
+		return(t)=>{
+			e.getElementsByClassName("code_out").item(0).innerHTML+=t.replace("\n","<br>");
+		};
     };
     var py_eventify=(e)=>{
-	let q=e;
-	console.log(q)
-	q.getElementsByClassName("code_run").item(0).addEventListener("click",()=>{
-	    let cdo=q.getElementsByClassName("code_out").item(0);
-	    cdo.innerHTML='';
-	    Sk.configure({output:out(q),read:builtinRead});
-	    let cde=q.getElementsByClassName("code_in").item(0);
-	    let cdsrc=cde.tagName==="DIV"?cde.innerHTML:cde.value;
-	    try{
-		Sk.importMainWithBody("<stdin>",false,cdsrc);
-	    }catch(aaa){
-		q.getElementsByClassName("code_out").item(0).innerHTML=aaa;
-	    }
-	    if(cdo.innerHTML==='')cdo.innerHTML='<br>';
-	});
-	let b=q.getElementsByClassName("code_delete")
-	if(b.length>0)
-	    b.item(0).addEventListener("click",()=>{
-		q.parentNode.removeChild(q);
-	    });
+		let q=e;
+		console.log(q)
+		q.getElementsByClassName("code_run").item(0).addEventListener("click",()=>{
+			let cdo=q.getElementsByClassName("code_out").item(0);
+			cdo.innerHTML='';
+			Sk.configure({output:out(q),read:builtinRead});
+			let cde=q.getElementsByClassName("code_in").item(0);
+			let cdsrc=cde.tagName==="DIV"?cde.innerHTML:cde.value;
+			try{
+				Sk.importMainWithBody("<stdin>",false,cdsrc);
+			}catch(aaa){
+				q.getElementsByClassName("code_out").item(0).innerHTML=aaa;
+			}
+			if(cdo.innerHTML==='')cdo.innerHTML='<br>';
+		});
+		let b=q.getElementsByClassName("code_delete")
+		if(b.length>0)
+			b.item(0).addEventListener("click",()=>{
+				q.parentNode.removeChild(q);
+			});
     };
     var mk_py_eventify=(e,daddy=document.getElementById("code_edits"))=>{
-	e.addEventListener("click",()=>{
-	    let ok=document.createElement('div');
-	    ok.setAttribute("class",'py_snip col-lg-4 col-md-6 col-sm-12');
-	    ok.innerHTML=document.getElementById("py_snip_edit_source").innerHTML;
-	    daddy.appendChild(ok);
-	    py_eventify(ok);
-	});
+		e.addEventListener("click",()=>{
+			let ok=document.createElement('div');
+			ok.setAttribute("class",'py_snip col-lg-4 col-md-6 col-sm-12');
+			ok.innerHTML=document.getElementById("py_snip_edit_source").innerHTML;
+			daddy.appendChild(ok);
+			py_eventify(ok);
+		});
     };
     var py_snips=document.getElementsByClassName("py_snip");
     for(var i=0;i<py_snips.length;i++){
-	py_eventify(py_snips.item(i));	
+		py_eventify(py_snips.item(i));	
     }
     var mk_py_snips=document.getElementsByClassName("new_py_snip");
     for(var i=0;i<mk_py_snips.length;i++){
-	mk_py_eventify(mk_py_snips.item(i));
+		mk_py_eventify(mk_py_snips.item(i));
     }
     var builtinRead=(x)=>{
-	if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
-	    throw "File not found: '" + x + "'";
-	return Sk.builtinFiles["files"][x];
+		if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
+			throw "File not found: '" + x + "'";
+		return Sk.builtinFiles["files"][x];
     };
     var stringify_snip=(e)=>{
-	let title=e.getElementsByClassName("code_title_inp").item(0);
-	let cde=e.getElementsByClassName("code_in").item(0);
-	let lang=e.getElementsByClassName("code_type").item(0);
-	return "{%{{{{"+lang.innerHTML.toLowerCase()+"\n"+title.value+"\n"+cde.value+"}}}}%}"
+		let title=e.getElementsByClassName("code_title_inp").item(0);
+		let cde=e.getElementsByClassName("code_in").item(0);
+		let lang=e.getElementsByClassName("code_type").item(0);
+		return "{%{{{{"+lang.innerHTML.toLowerCase().replace("\n","").replace("\r","")+"\n"+title.value.replace("\n","").replace("\r","")+"\n"+cde.value.replace("\n","").replace("\r","")+"}}}}%}"
     };
     var snippify_string=(s,daddy)=>{
-	var r=s.indexOf("{%{{{{")
-	var r1=s.indexOf("\n",r);
-	var r2=s.indexOf("\n",r1+1);
-	var nd=s.indexOf("}}}}%}",r2);
-	if(r==-1||r1==-1||r2==-1||nd==-1)return;
-	var lang=s.substring(r+6,r1);
-	var title=s.substring(r1+1,r2);
-	var body=s.substring(r2+1,nd);
-	if(lang=="py"){
-	    let ok=document.createElement('div');
-	    ok.setAttribute("class","py_snip col-lg-4 col-md-8 col-sm-12");
-	    ok.innerHTML=document.getElementById("py_snip_source").innerHTML;
-	    ok.getElementsByClassName("code_title_inp").item(0).innerHTML=title==""?lang+" code snippet":title;
-	    ok.getElementsByClassName("code_in").item(0).innerHTML=body;
-	    py_eventify(ok);
-	    daddy.appendChild(ok);
-	}
+		var r=s.indexOf("{%{{{{")
+		var r1=s.indexOf("\n",r);
+		var r2=s.indexOf("\n",r1+1);
+		var nd=s.indexOf("}}}}%}",r2);
+		if(r==-1||r1==-1||r2==-1||nd==-1)return;
+		var lang=s.substring(r+6,r1);
+		var title=s.substring(r1+1,r2);
+		var body=s.substring(r2+1,nd);
+		if(lang=="py"){
+			let ok=document.createElement('div');
+			ok.setAttribute("class","py_snip col-lg-4 col-md-8 col-sm-12");
+			ok.innerHTML=document.getElementById("py_snip_source").innerHTML;
+			ok.getElementsByClassName("code_title_inp").item(0).innerHTML=title==""?lang+" code snippet":title;
+			ok.getElementsByClassName("code_in").item(0).innerHTML=body;
+			py_eventify(ok);
+			daddy.appendChild(ok);
+		}
     }
-    document.getElementById("stringify_test").addEventListener("click",()=>{
-	snippify_string(stringify_snip(document.getElementsByClassName("py_snip").item(0)),document.getElementById("code_edits"));
-    });
+	var post=(path,params)=>{
+		const form = document.createElement('form');
+		form.method = 'post';
+		form.action = path;
+		for (const key in params) {
+			if (params.hasOwnProperty(key)) {
+				const hiddenField = document.createElement('input');
+				hiddenField.type = 'hidden';
+				hiddenField.name = key;
+				hiddenField.value = params[key];
+				form.appendChild(hiddenField);
+			}
+		}
+		document.body.appendChild(form);
+		form.submit();
+	}
+	var mkpost=()=>{
+		console.log("submitting");
+		var title=document.getElementById("new_post_title").value;
+		var body=document.getElementById("new_post_body").value;
+		var snips="";
+		var ok=document.getElementById("code_edits").children;
+		var parent=document.getElementById("parent").innerHTML;
+		var server=document.getElementById("server").innerHTML;
+		for(var i=0;i<ok.length;i++){
+			snips+=stringify_snip(ok.item(0));
+		}
+		post("/make_post",{"title":title,"body":body,"snips":snips,"parent":parent,"server":server})
+	}
+	document.getElementById("make_post").addEventListener("click",mkpost);
+    
 });
